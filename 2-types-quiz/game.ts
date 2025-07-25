f1ab84ae3fb933ab5323aa7c1302606068fc9d09
taewonnn <thisis_tw94@naver.com>
2025-07-25 10:03:33 +0900

    type - game

---
+++ 2-types-quiz/game.ts
@@ -0,0 +1,40 @@
+/**
+ * Let's make a game 🕹
+ */
+
+
+let position = {x:0, y:0};
+
+function move (dir:'up' | 'down' | 'left' | 'right') {
+  switch(dir) {
+    case 'up':
+      position.y += 1;
+      break;
+    
+    case 'down':
+      position.y += 1;
+      break;
+
+    case 'left':
+      position.x -= 1;
+      break;
+
+    case 'right':
+      position.x += 1;
+      break;
+
+    default: 
+      throw new Error('error!')
+  }
+}
+
+
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
