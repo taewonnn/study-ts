0ea440bbf07a7b6f9fc806c907ad03d2a7492664
taewonnn <thisis_tw94@naver.com>
2025-07-27 10:00:00 +0900

    intersection

---
+++ 1-types/1-7-intersection.ts
@@ -0,0 +1,25 @@
+{
+  /**
+   * Intersection Types: & <and>
+   */
+  type Student = {
+    name: string;
+    score: number;
+  };
+
+  type Worker = {
+    empolyeeId: number;
+    work: () => void;
+  };
+
+  function internWork(person: Student & Worker) {
+    console.log(person.name, person.empolyeeId, person.work());
+  }
+
+  internWork({
+    name: 'ellie',
+    score: 1,
+    empolyeeId: 123,
+    work: () => {},
+  });
+}
